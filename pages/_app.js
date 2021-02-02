d421e4642b1c34a7aff1efa2b33c890aa2bbb8b9
gerizim16 <gerizim16@gmail.com>
2021-02-02 20:11:41 +0800

    Changed default font to NotoSans

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,31 @@
+import Head from 'next/head'
 import '../styles/globals.scss'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/NotoSans/NotoSans-Regular.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/NotoSans/NotoSans-Bold.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/NotoSans/NotoSans-Italic.ttf"
+          as="font"
+          crossOrigin=""
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+
 }
