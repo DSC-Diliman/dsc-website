70dde5e4807dc6fb4a5f07886c558b463c6015c6
gerizim16 <gerizim16@gmail.com>
2021-02-03 02:21:11 +0800

    Updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,30 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        blue: '#4285F4',
-        red: '#EA4335',
-        green: '#0F9D58',
-        yellow: '#FBBC04',
+        blue: colors.blue[700],
+        red: colors.red[700],
+        green: colors.green[700],
+        yellow: colors.yellow[600],
       },
       fontSize: {
         '10xl': '14rem',
         'xxl': '18rem'
+      },
+      spacing: {
+        '88': '22rem',
+        'xl': '36rem',
+        '2xl': '42rem'
+      },
+      borderRadius: {
+        '4xl': '2rem'
+      },
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)'
       }
     },
     fontFamily: {
