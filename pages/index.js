1d833b3a49bab1b03274323ed1526b02e5646d61
gerizim16 <gerizim16@gmail.com>
2021-02-03 02:47:36 +0800

    Updated home page

--- pages/index.js
+++ pages/index.js
@@ -26,20 +26,20 @@ export default function Home() {
       </div>
 
       <div className="h-screen flex flex-col items-center justify-center">
-        <h1 className="text-3xl font-semibold m-7">What do we do in DSC UP Diliman?</h1>
+        <h1 className="text-xl md:text-3xl font-semibold mb-4 md:mb-8">What do we do in DSC UP Diliman?</h1>
 
-        <button className="btn-style1-red py-2 px-10 text-red border-2 border-red rounded-full mb-8">Check out our teams!</button>
+        <button className="btn-style1-red py-2 px-10 text-red border-2 border-red rounded-full mb-4 md:mb-8">Check out our teams!</button>
 
-        <div className="grid grid-cols-2 grid-rows-2 gap-16 mb-8">
+        <div className="grid grid-cols-1 grid-rows-4 md:grid-cols-2 md:grid-rows-2 gap-4 md:gap-16 mb-8">
           {[
             { heading: "Speaker Sessions", text: "Listen from esteemed speakers as they share their insights, experience and wisdom" },
             { heading: "Study Jams", text: "Learning never stops! Watch events online by Developer Student Clubs and by Google" },
             { heading: "Workshops", text: "Get a chance to learn new skills through hands-on workshops and study jams" },
             { heading: "Tech Solutions", text: "Use the skills you learn in our events to solve community problems through projects" }
-          ].map(({ heading, text }) => (
-            <div className="relative w-88 rounded-4xl shadow-3xl bg-white p-11">
-              <h1 className="mb-4 font-semibold text-xl">{heading}</h1>
-              <p>{text}</p>
+          ].map(({ heading, text }, index) => (
+            <div className="w-64 md:w-88 rounded-2xl md:rounded-4xl shadow-3xl bg-white p-4 md:p-11">
+              <h1 className="mb-1 md:mb-4 font-semibold text-lg md:text-xl">{heading}</h1>
+              <p className="text-sm md:text-base">{text}</p>
             </div>
           ))}
         </div>
