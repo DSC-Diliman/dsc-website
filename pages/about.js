5aae5a1a4df326b959e5c16ebb7ef8a2aa5dae8c
gerizim16 <gerizim16@gmail.com>
2021-02-03 16:05:54 +0800

    Updated about page

--- pages/about.js
+++ pages/about.js
@@ -11,11 +11,18 @@ export default function About() {
           height={214}
         />
       </div>
-      <h1>
-        About
-      </h1>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit
-      <div className="test2">test</div>
+      <div className="flex flex-col flex-nowrap items-center text-center pt-24">
+        <h1 className="mb-4 text-4xl">What is DSC-UPD?</h1>
+        <p className="mb-8 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        <div className="mb-8 max-w-7xl shadow-2xl overflow-hidden rounded-4xl">
+          <Image
+            src="/images/oops.png"
+            width={1203}
+            height={434}
+          />
+        </div>
+        <p className="max-w-3xl text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
     </Layout>
   )
 }
