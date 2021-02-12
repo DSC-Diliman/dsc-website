fe82da71de289921c2e3614822da960659c13f2a
gerizim16 <gerizim16@gmail.com>
2021-02-12 20:42:56 +0800

    Updated about page

--- pages/about.js
+++ pages/about.js
@@ -5,6 +5,7 @@ import ParallaxImage from "../components/parallax-image"
 // import { ScrollTrigger } from "gsap/dist/ScrollTrigger"
 import AnimatedObject from "../components/animated-object"
 import AnimatedEm from "../components/animated-em"
+import ParallaxObject from "../components/parallax-object"
 
 export default function About() {
   /* ScrollTrigger.defaults({
@@ -37,20 +38,9 @@ export default function About() {
             >
               <img src="/images/vision-1.png" />
             </AnimatedObject>
-            <AnimatedObject
-              className="div-style1 img-frame w-64 h-52"
-              fromVars={{
-                x: -100,
-                opacity: 0
-              }}
-              toVars={{
-                x: 0,
-                opacity: 1
-              }}
-              scrub={2}
-            >
+            <div className="div-style1 img-frame w-64 h-52">
               <img src="/images/vision-2.png" />
-            </AnimatedObject>
+            </div>
             <div className="mt-20 mr-20 ml-auto max-w-min text-5xl font-medium space-y-6">
               <AnimatedText className="text-red-400">Empower.</AnimatedText>
               <AnimatedText className="text-blue-400">Enlighten.</AnimatedText>
@@ -61,37 +51,50 @@ export default function About() {
             <h1 className="mt-28 text-lg text-red-700 font-normal mb-4">VISION</h1>
             <p className="text-3xl font-medium mb-6">Uplifting communities through <AnimatedEm emClassName="bg-yellow-300 bottom-0 left-4">technology</AnimatedEm></p>
             <p className="mb-12">This organization envisions itself as a community of tech enthusiasts who are passionate about uplifting communities through technology and innovation.</p>
-            <div className="img-frame div-style1 h-96 w-72">
+            <AnimatedObject
+              className="img-frame div-style1 h-96 w-72"
+              fromVars={{
+                x: 100,
+                opacity: 0
+              }}
+              toVars={{
+                x: 0,
+                opacity: 1
+              }}
+              scrub={2}
+            >
               <img src="/images/vision-3.png" />
-            </div>
+            </AnimatedObject>
           </div>
         </div>
       </section>
 
       <section id="mission" className="p-10 text-center">
         <h1 className="text-lg text-red-700 font-normal">MISSION</h1>
-        <p>We, as an organization, aim to</p>
+        <p className="mb-6">We, as an organization, aim to</p>
         <div className="flex justify-center">
           {[
             {
               title: "Empower",
               desc: "Empower people through technology and programming education",
-              image: ""
+              image: "/images/mission-1.png"
             },
             {
               title: "Enlighten",
               desc: "Equip members with the right tools to address local issues through data",
-              image: ""
+              image: "/images/mission-2.png"
             },
             {
               title: "Create",
               desc: "Create meaningful technological solutions for the community",
-              image: ""
+              image: "/images/mission-3.png"
             },
           ].map(e =>
             <div key={e.title} className="px-4 py-4 max-w-sm">
-              <img src={e.image} />
-              <p className="text-2xl font-medium">{e.title}</p>
+              <div className="mx-auto img-frame rounded-full mb-10 w-32 h-32">
+                <img src={e.image} />
+              </div>
+              <p className="text-2xl font-medium mb-2">{e.title}</p>
               <p>{e.desc}</p>
             </div>)}
         </div>
@@ -99,9 +102,12 @@ export default function About() {
 
       <section id="reach" className="relative px-10 py-10 bg-gray-200 overflow-hidden">
         <div className="mx-auto max-w-max grid grid-cols-4 grid-rows-2 gap-8">
-          <div className="flex flex-col justify-center z-0">
+          <div className="relative flex flex-col justify-center z-0">
             <h1>Reach Us</h1>
             <p className="text-sm">in our socials!</p>
+            <ParallaxObject className="absolute -left-6 -z-10 w-20 h-20 bg-yellow-300 rounded-full" y={[-200, 200]} />
+            <ParallaxObject className="absolute left-12 -z-10 w-10 h-10 bg-blue-300 rounded-full" y={[-200, 450]} />
+            <ParallaxObject className="absolute left-36 -z-10 w-10 h-10 bg-red-300 rounded-full" y={[-100, 100]} />
           </div>
           <div />
           {[
