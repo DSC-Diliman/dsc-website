47e4cef16109781f39f66de359d914de8a3227ab
gerizim16 <gerizim16@gmail.com>
2021-02-12 20:42:36 +0800

    Added parallax object component

---
+++ components/parallax-object.js
@@ -0,0 +1,28 @@
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger"
+import { useEffect, useRef } from "react"
+
+gsap.registerPlugin(ScrollTrigger)
+
+export default function ParallaxObject({ children, className, y = [0, 0], x = [0, 0] }) {
+  const ref = useRef(null)
+
+  useEffect(() => {
+    gsap.fromTo(ref.current, {
+      y: y[0],
+      x: x[0]
+    }, {
+      y: y[1],
+      x: x[1],
+      ease: "none",
+      scrollTrigger: {
+        trigger: ref.current,
+        scrub: true
+      }
+    })
+  })
+
+  return <div className={className} ref={ref}>
+    {children}
+  </div>
+}
