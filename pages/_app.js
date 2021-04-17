4e00e72254a8202aface9a72c86751f5ec5f36d9
gerizim16 <gerizim16@gmail.com>
2021-04-17 21:55:03 +0800

    Font optimization No need for preload

--- pages/_app.js
+++ pages/_app.js
@@ -8,30 +8,30 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link
+        {/* <link
           rel="preload"
           href="/fonts/GoogleSans/GoogleSans-Regular.woff"
           as="font"
-          crossOrigin=""
+          crossOrigin="anonymous"
         />
         <link
           rel="preload"
           href="/fonts/GoogleSans/GoogleSans-Medium.woff"
           as="font"
-          crossOrigin=""
+          crossOrigin="anonymous"
         />
         <link
           rel="preload"
           href="/fonts/GoogleSans/GoogleSans-Bold.woff"
           as="font"
-          crossOrigin=""
+          crossOrigin="anonymous"
         />
         <link
           rel="preload"
           href="/fonts/GoogleSans/GoogleSans-Italic.woff"
           as="font"
-          crossOrigin=""
-        />
+          crossOrigin="anonymous"
+        /> */}
       </Head>
       {getLayout(<Component {...pageProps}></Component>)}
     </>
