837d9b4c08c38c6ba7164bb4a422c2bae0821504
Daryll Ko <daryll.ko@gmail.com>
2023-01-18 15:54:55 +0800

    fix: clean `pages/_app`

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,30 +9,6 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="shortcut icon" href="/favicon.png" />
-        {/* <link
-          rel="preload"
-          href="/fonts/GoogleSans/GoogleSans-Regular.woff"
-          as="font"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/GoogleSans/GoogleSans-Medium.woff"
-          as="font"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/GoogleSans/GoogleSans-Bold.woff"
-          as="font"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/GoogleSans/GoogleSans-Italic.woff"
-          as="font"
-          crossOrigin="anonymous"
-        /> */}
       </Head>
       {getLayout(<Component {...pageProps}></Component>)}
     </>
