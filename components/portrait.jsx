17f0cbf7fad784fabac1114686fad709c622d8c9
Daryll Ko <daryll.ko@gmail.com>
2023-01-18 15:03:02 +0800

    refactor: use `&&` instead of ternary and `null`

--- components/portrait.jsx
+++ components/portrait.jsx
@@ -11,10 +11,10 @@ export default function Portrait({ className, src, name, position, desc, socials
         <p className="text-xs">{position}</p>
         <div className="text-xs pt-3 md:text-sm flex flex-col flex-1"><p className="my-auto">{desc}</p></div>
         <div className="text-2xl md:text-4xl">
-          {socials.facebook ? <a href={socials.facebook} target="_blank"><TiSocialFacebookCircular /></a> : null}
-          {socials.twitter ? <a href={socials.twitter} target="_blank"><TiSocialTwitterCircular /></a> : null}
-          {socials.linkedin ? <a href={socials.linkedin} target="_blank"><TiSocialLinkedinCircular /></a> : null}
-          {socials.github ? <a href={socials.github} target="_blank"><TiSocialGithubCircular /></a> : null}
+          {socials.facebook && <a href={socials.facebook} target="_blank"><TiSocialFacebookCircular /></a>}
+          {socials.twitter && <a href={socials.twitter} target="_blank"><TiSocialTwitterCircular /></a>}
+          {socials.linkedin && <a href={socials.linkedin} target="_blank"><TiSocialLinkedinCircular /></a>}
+          {socials.github && <a href={socials.github} target="_blank"><TiSocialGithubCircular /></a>}
         </div>
       </div>
     </div>
