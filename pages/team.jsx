59fa999c9af1a494e49b195b18e5aa5018215108
Daryll Ko <daryll.ko@gmail.com>
2023-01-18 21:30:52 +0800

    feat: minor edits to `Meet our Team` intro

--- pages/team.jsx
+++ pages/team.jsx
@@ -34,7 +34,7 @@ export default function Team({allExecData, allTechData, allOpsData, allCommsData
     <>
       <div className="px-4 md:px-10 pt-12 md:pt-24 pb-8 w-full text-center">
         <h1 className="my-4">Meet our Team</h1>
-        <p>Our lineup that empowers, enlightens, and nurtures student developers in UP Diliman</p>
+        <p>The lineup that empowers, enlightens, and nurtures student developers in UP Diliman!</p>
       </div>
       <div className="space-y-14 md:space-y-20 pb-20 px-4 md:px-10 overflow-x-hidden">
         <section id="executive">
