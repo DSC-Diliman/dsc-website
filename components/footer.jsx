7f8a5f9dcf1e736068d4125d197831750bb3622f
Daryll Ko <daryll.ko@gmail.com>
2023-01-18 20:41:19 +0800

    refactor: use `next/image` for stacked logo

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,12 +1,14 @@
+import Image from "next/image"
+
 export default function Footer({ children }) {
   return (
     <footer className="flex flex-col content-center space-y-2 py-6 md:py-10 text-xs md:text-sm text-center">
       <div className="flex-initial flex flex-col items-center pb-2">
-        <img
+        <Image
           src="/images/dsc-upd-logo-stacked-small.png"
           alt="Google Developer Student Clubs UP Diliman Stacked Logo"
-          height="69px"
-          width="200px"
+          height={69}
+          width={200}
         />
       </div>
       <p className="text-xs">2023 Google Developer Student Clubs UP Diliman.</p>
