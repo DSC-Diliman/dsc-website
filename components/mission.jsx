5c99b7d6b5e3dbaf99c622f578e12824fe674dc3
Daryll Ko <daryll.ko@gmail.com>
2023-01-20 14:53:11 +0800

    fix: adjust spacing for components under `/about`

--- components/mission.jsx
+++ components/mission.jsx
@@ -2,36 +2,36 @@ import Image from "next/image"
 
 export default function Mission() {
 	return <section id="mission" className="p-4 md:p-10 text-center">
-        <h1 className="text-lg text-red-700 font-black">MISSION</h1>
-        <p className="mb-6">We, as an organization, aim to</p>
-        <div className="flex justify-center">
-          {[
-            {
-              title: "Empower",
-              desc: "people through technology and programming education",
-              image: "/images/dot_empower.png"
-            },
-            {
-              title: "Enlighten",
-              desc: "members with the right tools to address local issues through data",
-              image: "/images/dot_enlighten.png"
-            },
-            {
-              title: "Create",
-              desc: "meaningful technological solutions for the community",
-              image: "/images/dot_create.png"
-            },
-          ].map(e =>
-            <div key={e.title} className="px-1 md:px-4 py-4 max-w-sm">
-              <div 
-                className="mx-auto img-frame rounded-full mb-10 w-20 h-20 sm:w-32 sm:h-32"
-                style={{ position: "relative" }}
-              >
-                <Image src={e.image} height={480} width={480} />
-              </div>
-              <p className="text-lg md:text-2xl font-medium mb-2">{e.title}</p>
-              <p className="text-xs sm:text-base">{e.desc}</p>
-            </div>)}
-        </div>
-      </section>
+		<h1 className="text-lg text-red-700 font-black">MISSION</h1>
+		<p className="mb-6">We, as an organization, aim to</p>
+		<div className="flex justify-center">
+			{[
+				{
+					title: "Empower",
+					desc: "people through technology and programming education",
+					image: "/images/dot_empower.png"
+				},
+				{
+					title: "Enlighten",
+					desc: "members with the right tools to address local issues through data",
+					image: "/images/dot_enlighten.png"
+				},
+				{
+					title: "Create",
+					desc: "meaningful technological solutions for the community",
+					image: "/images/dot_create.png"
+				},
+			].map(e =>
+				<div key={e.title} className="px-1 md:px-4 py-4 max-w-sm">
+					<div 
+						className="mx-auto img-frame rounded-full mb-10 w-20 h-20 sm:w-32 sm:h-32"
+						style={{ position: "relative" }}
+					>
+						<Image src={e.image} height={480} width={480} />
+					</div>
+					<p className="text-lg md:text-2xl font-medium mb-2">{e.title}</p>
+					<p className="text-xs sm:text-base">{e.desc}</p>
+				</div>)}
+		</div>
+	</section>
 }
