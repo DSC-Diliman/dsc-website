d40827e305fea4063ee69f312def3a0f9e3df89a
Daryll Ko <daryll.ko@gmail.com>
2023-01-20 15:16:00 +0800

    refactor: extract Operations list into its own component

--- pages/team.jsx
+++ pages/team.jsx
@@ -11,6 +11,7 @@ import extractPortraits from "../lib/extractPortraits"
 import markdownToHtml from "/lib/markdownToHtml"
 import ExecutiveTeam from "../components/executive-team"
 import TechnologyTeam from "../components/technology-team"
+import OperationsTeam from "../components/operations-team"
 
 export default function Team({allExecData, allTechData, allOpsData, allCommsData, allExteFinData, allWebDevData}) {
   return (
@@ -22,18 +23,8 @@ export default function Team({allExecData, allTechData, allOpsData, allCommsData
       <div className="space-y-14 md:space-y-20 pb-20 px-4 md:px-10 overflow-x-hidden">
 				<ExecutiveTeam allExecData={allExecData} />
 				<TechnologyTeam allTechData={allTechData} />
-        <section id="operations" className="grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 justify-items-center max-w-5xl mx-auto gap-8">
-          <div className="pt-32">
-            <h2><AnimatedEm emClassName="bg-red-400 bottom-0 -left-4">Operations</AnimatedEm></h2>
-            <p className="mt-4">
-              The Operations Department oversees the operations of the organization and manages event, ad hoc projects, recruitment, and member welfare.
-            </p>
-          </div>
-          <div className="relative w-full flex flex-wrap justify-evenly gap-4">
-            <ParallaxObject className="absolute -right-12 w-20 h-20 bg-red-400 rounded-full" y={[0, 200]} end="bottom+=160 top" />
-            {extractPortraits(allOpsData)}
-          </div>
-        </section>
+				<OperationsTeam allOpsData={allOpsData} />
+
         <section id="communications" className="grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 justify-items-center max-w-5xl mx-auto gap-8">
           <div className="md:order-2">
             <h2><AnimatedEm emClassName="bg-blue-400 bottom-0 left-4">Communications</AnimatedEm></h2>
