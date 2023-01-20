14f79da5d81b7f8105f2a8250e735703825fac0f
Daryll Ko <daryll.ko@gmail.com>
2023-01-20 16:26:02 +0800

    refactor: use `Link` components in sitemap

--- components/link-group.jsx
+++ components/link-group.jsx
@@ -1,10 +1,16 @@
+import Link from "next/link"
+
 export default function LinkGroup({head, links}) {
 	const linksJSX = links.map(({text, href}) => (
-		<a className="block py-2.5" href={`${head.href}#${href}`}>{text}</a>
+		<Link href={`${head.href}#${href}`}>
+			<a className="block py-2.5" href={`${head.href}#${href}`}>{text}</a>
+		</Link>
 	))
 	return (
 		<div className="text-left">
-			<a className="font-bold block py-2.5" href={`${head.href}`}>{head.text}</a>
+			<Link href={`${head.href}`}>
+				<a className="font-bold block py-2.5" href={`${head.href}`}>{head.text}</a>
+			</Link>
 			{linksJSX}
 		</div>
 	)
