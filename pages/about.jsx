9aed21176b88188277cb0bf60a3ba59e7bcebeb0
Daryll Ko <daryll.ko@gmail.com>
2023-01-20 14:36:00 +0800

    refactor: extract Socials into its own component

--- pages/about.jsx
+++ pages/about.jsx
@@ -4,7 +4,7 @@ import AnimatedText from "../components/animated-text"
 import ParallaxImage from "../components/parallax-image"
 import AnimatedObject from "../components/animated-object"
 import AnimatedEm from "../components/animated-em"
-import ParallaxObject from "../components/parallax-object"
+import Socials from '../components/socials'
 
 export default function About() {
   return (
@@ -101,36 +101,7 @@ export default function About() {
         </div>
       </section>
 
-      <section id="reach" className="relative px-10 py-10 bg-gray-200 overflow-hidden">
-        <div className="mx-auto max-w-max grid grid-cols-2 sm:grid-cols-4 gap-8">
-          <div className="col-span-2 sm:col-span-1 relative flex flex-col justify-center">
-            <ParallaxObject className="absolute -left-6 w-20 h-20 bg-yellow-300 rounded-full" y={[-200, 200]} />
-            <ParallaxObject className="absolute left-12 w-10 h-10 bg-blue-300 rounded-full" y={[-200, 450]} />
-            <ParallaxObject className="absolute left-36 w-10 h-10 bg-red-300 rounded-full" y={[-100, 100]} />
-            <h1 className="relative">Reach us</h1>
-            <p className="relative text-sm">in our socials!</p>
-          </div>
-          {[
-            { name: "Facebook", icon: "/images/facebook-icon.svg", link: "https://www.facebook.com/GDSCupdiliman" },
-            { name: "LinkedIn", icon: "/images/linkedin-icon.svg", link: "https://www.linkedin.com/company/gdscupdiliman/mycompany/" },
-          ].map((e, index) => (
-            e ?
-              <a className="relative" key={index} href={e.link} target="_blank" >
-                <button 
-                  className="w-full px-3 py-3 md:px-10 md:py-7 text-center transition-shadow hover:shadow-none div-style1"
-                >
-                  <img
-                    src={e.icon}
-                    className="h-6 md:h-8 mx-auto mb-3"
-                  />
-                  <p className="text-sm">{e.name}</p>
-                </button>
-              </a>
-              :
-              <div key={index} className="hidden sm:block" />
-          ))}
-        </div>
-      </section>
+			<Socials />
     </>
   )
 }
