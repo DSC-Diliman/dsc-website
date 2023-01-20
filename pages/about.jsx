ee8f7ead9f512ad00b585b217bc6a86841aeb390
Daryll Ko <daryll.ko@gmail.com>
2023-01-20 14:44:33 +0800

    refactor: extract Mission into its own component

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Layout from "../components/layout-default"
+import Mission from '../components/mission'
 import ParallaxImage from "../components/parallax-image"
 import Socials from '../components/socials'
 import Vision from '../components/vision'
@@ -17,40 +18,7 @@ export default function About() {
         </p>
       </section>
 			<Vision />
-      <section id="mission" className="p-4 md:p-10 text-center">
-        <h1 className="text-lg text-red-700 font-black">MISSION</h1>
-        <p className="mb-6">We, as an organization, aim to</p>
-        <div className="flex justify-center">
-          {[
-            {
-              title: "Empower",
-              desc: "people through technology and programming education",
-              image: "/images/dot_empower.png"
-            },
-            {
-              title: "Enlighten",
-              desc: "members with the right tools to address local issues through data",
-              image: "/images/dot_enlighten.png"
-            },
-            {
-              title: "Create",
-              desc: "meaningful technological solutions for the community",
-              image: "/images/dot_create.png"
-            },
-          ].map(e =>
-            <div key={e.title} className="px-1 md:px-4 py-4 max-w-sm">
-              <div 
-                className="mx-auto img-frame rounded-full mb-10 w-20 h-20 sm:w-32 sm:h-32"
-                style={{ position: "relative" }}
-              >
-                <Image src={e.image} height={480} width={480} />
-              </div>
-              <p className="text-lg md:text-2xl font-medium mb-2">{e.title}</p>
-              <p className="text-xs sm:text-base">{e.desc}</p>
-            </div>)}
-        </div>
-      </section>
-
+			<Mission />
 			<Socials />
     </>
   )
