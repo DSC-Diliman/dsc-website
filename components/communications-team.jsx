365539b79e89ff01c28af22e134235d0977d1370
Daryll Ko <daryll.ko@gmail.com>
2023-01-20 15:17:57 +0800

    refactor: extract Communications team list into its own component

---
+++ components/communications-team.jsx
@@ -0,0 +1,14 @@
+export default function CommunicationsTeam({allCommsData}) {
+	return <section id="communications" className="grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 justify-items-center max-w-5xl mx-auto gap-8">
+		<div className="md:order-2">
+			<h2><AnimatedEm emClassName="bg-blue-400 bottom-0 left-4">Communications</AnimatedEm></h2>
+			<p className="mt-4">
+				The Communications Department is in charge of crafting promotional plans and publicity materials for the organization’s projects and initiatives, and managing all of the organization’s social media pages.
+			</p>
+		</div>
+		<div className="relative w-full flex flex-wrap justify-evenly gap-4">
+			<ParallaxObject className="absolute -left-2 w-10 h-10 bg-blue-400 rounded-full" y={[-100, 200]} end="bottom+=160 top" />
+			{extractPortraits(allCommsData)}
+		</div>
+	</section>
+}
--- pages/team.jsx
+++ pages/team.jsx
@@ -12,6 +12,7 @@ import markdownToHtml from "/lib/markdownToHtml"
 import ExecutiveTeam from "../components/executive-team"
 import TechnologyTeam from "../components/technology-team"
 import OperationsTeam from "../components/operations-team"
+import CommunicationsTeam from "../components/communications-team"
 
 export default function Team({allExecData, allTechData, allOpsData, allCommsData, allExteFinData, allWebDevData}) {
   return (
@@ -24,19 +25,8 @@ export default function Team({allExecData, allTechData, allOpsData, allCommsData
 				<ExecutiveTeam allExecData={allExecData} />
 				<TechnologyTeam allTechData={allTechData} />
 				<OperationsTeam allOpsData={allOpsData} />
+				<CommunicationsTeam allCommsData={allCommsData} />
 
-        <section id="communications" className="grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 justify-items-center max-w-5xl mx-auto gap-8">
-          <div className="md:order-2">
-            <h2><AnimatedEm emClassName="bg-blue-400 bottom-0 left-4">Communications</AnimatedEm></h2>
-            <p className="mt-4">
-              The Communications Department is in charge of crafting promotional plans and publicity materials for the organization’s projects and initiatives, and managing all of the organization’s social media pages.
-            </p>
-          </div>
-          <div className="relative w-full flex flex-wrap justify-evenly gap-4">
-            <ParallaxObject className="absolute -left-2 w-10 h-10 bg-blue-400 rounded-full" y={[-100, 200]} end="bottom+=160 top" />
-            {extractPortraits(allCommsData)}
-          </div>
-        </section>
         <section id="finance-and-externals" className="grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 justify-items-center max-w-5xl mx-auto gap-8">
           <div>
             <h2><AnimatedEm emClassName="bg-yellow-300 bottom-0 -left-4" trigger="90%">Finance and Externals</AnimatedEm></h2>
