233120c1326b1233da6be3f41a90bbf757b7c006
Daryll Ko <daryll.ko@gmail.com>
2023-01-20 14:42:33 +0800

    refactor: extract Vision into its own component

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,10 +1,8 @@
 import Image from 'next/image'
 import Layout from "../components/layout-default"
-import AnimatedText from "../components/animated-text"
 import ParallaxImage from "../components/parallax-image"
-import AnimatedObject from "../components/animated-object"
-import AnimatedEm from "../components/animated-em"
 import Socials from '../components/socials'
+import Vision from '../components/vision'
 
 export default function About() {
   return (
@@ -18,55 +16,7 @@ export default function About() {
           having 41 of those chapters. In other words, the GDSC UP Diliman Chapter is part of a <b>global community</b>.
         </p>
       </section>
-
-      <section id="vision" className="px-4 py-10 md:p-10 bg-gray-200 overflow-hidden">
-        <div className="grid sm:grid-cols-2 max-w-3xl mx-auto gap-4 md:gap-12">
-          <div className="hidden sm:block">
-            <AnimatedObject
-              className="img-frame div-style1 ml-auto w-56 h-64 mb-16"
-              fromVars={{
-                x: -100,
-                opacity: 0
-              }}
-              toVars={{
-                x: 0,
-                opacity: 1
-              }}
-              scrub={2}
-            >
-              <Image src="/images/vision-1.png" layout="fill"/>
-            </AnimatedObject>
-            <div className="div-style1 img-frame w-64 h-52" style={{ position: "relative" }}>
-              <Image src="/images/vision-2.png" layout="fill"/>
-            </div>
-            <div className="mt-20 mr-20 ml-auto max-w-min text-5xl font-medium space-y-6">
-              <AnimatedText className="text-red-400">Empower.</AnimatedText>
-              <AnimatedText className="text-blue-400">Enlighten.</AnimatedText>
-              <AnimatedText className="text-yellow-400">Create.</AnimatedText>
-            </div>
-          </div>
-          <div>
-            <h1 className="mb-4 sm:mt-28 text-lg text-red-700 font-black">VISION</h1>
-            <p className="text-3xl font-medium mb-6">Uplifting communities through <AnimatedEm emClassName="bg-yellow-300 bottom-0 left-4">technology</AnimatedEm></p>
-            <p className="sm:mb-12">Our organization envisions itself as a community of tech enthusiasts who are passionate about uplifting communities through technology and innovation.</p>
-            <AnimatedObject
-              className="hidden sm:block img-frame div-style1 h-96 w-85"
-              fromVars={{
-                x: 100,
-                opacity: 0
-              }}
-              toVars={{
-                x: 0,
-                opacity: 1
-              }}
-              scrub={2}
-            >
-              <Image src="/images/vision-3.png" layout="fill"/>
-            </AnimatedObject>
-          </div>
-        </div>
-      </section>
-
+			<Vision />
       <section id="mission" className="p-4 md:p-10 text-center">
         <h1 className="text-lg text-red-700 font-black">MISSION</h1>
         <p className="mb-6">We, as an organization, aim to</p>
