95299f0b69cbd83a8e4e9e93983a66e6b1b53d2c
Daryll Ko <daryll.ko@gmail.com>
2023-01-20 15:03:35 +0800

    refactor: extract Tech team list into its own component

--- pages/team.jsx
+++ pages/team.jsx
@@ -10,6 +10,7 @@ import { getExteFinData } from "/lib/posts"
 import extractPortraits from "../lib/extractPortraits"
 import markdownToHtml from "/lib/markdownToHtml"
 import ExecutiveList from "../components/executive-list"
+import TechnologyTeam from "../components/technology-team"
 
 export default function Team({allExecData, allTechData, allOpsData, allCommsData, allExteFinData, allWebDevData}) {
   return (
@@ -20,19 +21,7 @@ export default function Team({allExecData, allTechData, allOpsData, allCommsData
       </div>
       <div className="space-y-14 md:space-y-20 pb-20 px-4 md:px-10 overflow-x-hidden">
 				<ExecutiveList allExecData={allExecData} />
-        <section id="technology" className="grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 justify-items-center max-w-5xl mx-auto gap-8">
-          <div className="md:order-2 pt-32">
-            <h2><AnimatedEm emClassName="bg-green-500 bottom-0 left-4">Technology</AnimatedEm></h2>
-            <p className="mt-4">
-              The Technology Department handles most of the organizational functions that require specialized knowledge in programming and technology.
-            </p>
-          </div>
-          <div className="relative w-full flex flex-wrap justify-evenly gap-4">
-            <ParallaxObject className="absolute -left-24 w-20 h-20 bg-green-500 rounded-full" y={[0, 200]} end="bottom+=160 top" />
-            <ParallaxObject className="absolute bottom-0 right-24 w-6 h-6 bg-green-500 rounded-full" y={[0, 50]} end="bottom+=200 top" />
-            {extractPortraits(allTechData)}
-          </div>
-        </section>
+				<TechnologyTeam allTechData={allTechData} />
         <section id="operations" className="grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 justify-items-center max-w-5xl mx-auto gap-8">
           <div className="pt-32">
             <h2><AnimatedEm emClassName="bg-red-400 bottom-0 -left-4">Operations</AnimatedEm></h2>
