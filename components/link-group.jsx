3b34a45dc4b853ae8e53f08df477c8e3f763c446
Daryll Ko <daryll.ko@gmail.com>
2023-01-20 17:47:46 +0800

    fix: make sitemap actually use `Link`s

--- components/link-group.jsx
+++ components/link-group.jsx
@@ -3,13 +3,13 @@ import Link from "next/link"
 export default function LinkGroup({head, links}) {
 	const linksJSX = links.map(({text, href}) => (
 		<Link href={`${head.href}#${href}`}>
-			<a className="block py-2.5" href={`${head.href}#${href}`}>{text}</a>
+			<p className="block py-2.5 cursor-pointer" href={`${head.href}#${href}`}>{text}</p>
 		</Link>
 	))
 	return (
 		<div className="text-left">
 			<Link href={`${head.href}`}>
-				<a className="font-bold block py-2.5" href={`${head.href}`}>{head.text}</a>
+				<p className="font-bold block py-2.5 cursor-pointer" href={`${head.href}`}>{head.text}</p>
 			</Link>
 			{linksJSX}
 		</div>
