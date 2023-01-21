dfe361f2edaab82a91a76c42b1e9681fd5a28fd3
Daryll Ko <daryll.ko@gmail.com>
2023-01-22 01:41:54 +0800

    feat!: add Year selector in `team` page

--- pages/team.jsx
+++ pages/team.jsx
@@ -1,5 +1,4 @@
 import Layout from "../components/layout-default"
-import markdownToHtml from "/lib/markdownToHtml"
 import ExecTeam from "../components/exec-team"
 import TechTeam from "../components/tech-team"
 import OpsTeam from "../components/ops-team"
@@ -7,22 +6,49 @@ import CommsTeam from "../components/comms-team"
 import FinExtTeam from "../components/finext-team"
 import WebDevTeam from "../components/web-dev-team"
 import { getTeam } from "../lib/posts"
+import { useState } from "react"
 
-export default function Team({allMemberData}) {
+export default function Team({ allMemberData }) {
+	const [yearToShow, setYearToShow] = useState(2022)
+
+	function getFilteredData(wantedYear, wantedTeam) {
+		return allMemberData.filter(({ year, team }) => year === wantedYear && team === wantedTeam)
+	}
+
   return (
     <>
       <div className="px-4 md:px-10 pt-12 md:pt-24 pb-8 w-full text-center">
         <h1 className="my-4">Meet our Team</h1>
         <p>The lineup that empowers, enlightens, and nurtures student developers in UP Diliman!</p>
+				<div className="mt-12 mb-6 flex justify-center gap-x-10">
+					{
+						[2022, 2021].map(year => (
+							<button
+								onClick={() => setYearToShow(year)}
+								className={
+									`
+									border-2 border-black rounded-full
+									text-2xl
+									px-8 py-2
+									${year === yearToShow && "bg-black text-white cursor-default"}
+									${year !== yearToShow && "hover:underline"}
+									`
+								}
+							>
+								{year} - {year + 1}
+							</button>
+							))
+						}
+				</div>
       </div>
       <div className="space-y-14 md:space-y-20 pb-20 px-4 md:px-10 overflow-x-hidden">
-				<ExecTeam allExecData={allExecData} />
-				<TechTeam allTechData={allTechData} />
-				<OpsTeam allOpsData={allOpsData} />
-				<CommsTeam allCommsData={allCommsData} />
-				<FinExtTeam allExteFinData={allExteFinData} />
+				<ExecTeam allExecData={getFilteredData(yearToShow, "Executive")} />
+				<TechTeam allTechData={getFilteredData(yearToShow, "Technology")} />
+				<OpsTeam allOpsData={getFilteredData(yearToShow, "Operations")} />
+				<CommsTeam allCommsData={getFilteredData(yearToShow, "Communications")} />
+				<FinExtTeam allExteFinData={getFilteredData(yearToShow, "Finance and Externals")} />
       </div>
-			<WebDevTeam allWebDevData={allWebDevData} />
+			<WebDevTeam allWebDevData={getFilteredData(yearToShow, "Web Development")} />
     </>
   )
 }
