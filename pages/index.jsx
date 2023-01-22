582a3c9d6850009a29affa04aa10c659033a340c
Daryll Ko <daryll.ko@gmail.com>
2023-01-22 15:32:47 +0800

    refactor: make styles readable in `pages/index`

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,13 +23,24 @@ export default function Home({ eventsArray }) {
   function renderOffsetGrid(elements) {
     const color_gen = getNextPrimaryColor()
     return (
-      <div className="grid grid-cols-1 grid-rows-4 sm:grid-cols-2 sm:grid-rows-2 gap-8 md:gap-16 mt-6 mb-40">
+      <div className="
+				grid grid-cols-1 grid-rows-4 gap-8 mt-6 mb-40
+				sm:grid-cols-2 sm:grid-rows-2
+				md:gap-16
+			">
         {elements.map(({ heading, text, icon }, index) => {
 					const color = color_gen.next().value
 					return (
-						<div key={index} className="relative w-64 div-style1 overflow-hidden md:w-80 sm:even:top-1/2 p-4 md:p-11">
-							<div className="mb-1 sm:mb-4 font-semibold text-lg sm:text-xl flex gap-5 items-center">
-								<AnimatedEm emClassName={`right-4 bottom-0 bg-${color}-300`} >{heading}</AnimatedEm>
+						<div key={index} className="
+							relative w-64 div-style1 overflow-hidden p-4
+							sm:even:top-1/2
+							md:w-80 md:p-11
+						">
+							<div className="
+								mb-1 font-semibold text-lg flex gap-5 items-center
+								sm:mb-4 sm:text-xl
+							">
+								<AnimatedEm emClassName={`right-4 bottom-0 bg-${color}-300`}>{heading}</AnimatedEm>
 							</div>
 							<p className="text-sm sm:text-base">{text}</p>
 							<IconContext.Provider value={
@@ -55,21 +66,47 @@ export default function Home({ eventsArray }) {
       </Head>
 			<Landing />
 
-      <section className="px-4 md:px-10 pt-12 pb-6 sm:pb-16 bg-gray-200 flex flex-col items-center justify-center md:shadow-above">
+      <section className="
+				px-4 pt-12 pb-6 bg-gray-200 flex flex-col items-center justify-center
+				sm:pb-16
+				md:px-10 md:shadow-above
+			">
         <h1 className="text-center mb-4">What do we do in GDSC UP Diliman?</h1>
         {renderOffsetGrid([
-          { heading: "Speaker Sessions", text: "Hear from esteemed speakers as they share their insights, experience, and wisdom.", icon: <GiMicrophone /> },
-          { heading: "Internal", text: "Get a glimpse of how our organization operates!", icon: <GiMagnifyingGlass /> },
-          { heading: "Workshops", text: "Get a chance to learn new skills through hands-on workshops and study jams.", icon: <BsWrench />  },
-          { heading: "Tech Solutions", text: "Use the skills you learn in our events to solve community problems through projects!", icon: <ImLab /> }
+          {
+						heading: "Speaker Sessions",
+						text: "Hear from esteemed speakers as they share their insights, experience, and wisdom.",
+						icon: <GiMicrophone />
+					},
+          {
+						heading: "Internal",
+						text: "Get a glimpse of how our organization operates!",
+						icon: <GiMagnifyingGlass />
+					},
+          {
+						heading: "Workshops",
+						text: "Get a chance to learn new skills through hands-on workshops and study jams.",
+						icon: <BsWrench />
+					},
+          {
+						heading: "Tech Solutions",
+						text: "Use the skills you learn in our events to solve community problems through projects!",
+						icon: <ImLab />
+					},
         ])}
         <Link href="/team">
           <button className="btn-style2-red">Check out our teams!</button>
         </Link>
       </section>
 
-      <section className="p-4 md:px-10 pt-10 pb-14 md:py-20">
-        <div className="mx-auto md:max-w-2xl flex flex-col items-center">
+      <section className="
+				py-4 pt-10 pb-14
+				md:px-10 md:py-20
+			">
+        <div className="
+					mx-auto flex flex-col items-center
+					md:max-w-2xl
+				">
           <h1 className="mb-4 text-center">Upcoming Events</h1>
           <div className="div-style1 w-full overflow-hidden mt-10 mb-20">
             <EventListView
