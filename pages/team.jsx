fd5ff89e08492784f1193a0b3afed53ad69d3419
Daryll Ko <daryll.ko@gmail.com>
2023-01-22 14:23:00 +0800

    fix: make pill in team year selection slightly shorter

--- pages/team.jsx
+++ pages/team.jsx
@@ -29,7 +29,7 @@ export default function Team({ allMemberData }) {
 									`
 									border-2 border-black rounded-full
 									text-2xl
-									px-8 py-2
+									px-8 py-1
 									${year === yearToShow && "bg-black text-white cursor-default"}
 									${year !== yearToShow && "hover:underline"}
 									`
