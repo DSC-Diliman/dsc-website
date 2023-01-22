fac77ae17b7309ef35fdbdb902151f8e3faf55ba
Daryll Ko <daryll.ko@gmail.com>
2023-01-22 18:40:40 +0800

    fix: make cursor remain at pointer for team year selection buttons

--- pages/team.jsx
+++ pages/team.jsx
@@ -30,7 +30,7 @@ export default function Team({ allMemberData }) {
 									border-2 border-black rounded-full
 									text-2xl
 									px-8 py-1
-									${year === yearToShow && "bg-gradient-to-r from-blue-200 to-green-200 cursor-default"}
+									${year === yearToShow && "bg-gradient-to-r from-blue-200 to-green-200"}
 									${year !== yearToShow && "hover:underline"}
 									`
 								}
