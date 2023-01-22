4df512878c22a5bf7f93905fa7a8c652f8649891
Daryll Ko <daryll.ko@gmail.com>
2023-01-22 14:53:32 +0800

    fix: add `key` prop wherever necessary

--- components/link-group.jsx
+++ components/link-group.jsx
@@ -1,8 +1,8 @@
 import Link from "next/link"
 
 export default function LinkGroup({head, links}) {
-	const linksJSX = links.map(({text, href}) => (
-		<Link href={`${head.href}#${href}`}>
+	const linksJSX = links.map(({text, href}, index) => (
+		<Link key={index} href={`${head.href}#${href}`}>
 			<p className="block py-2.5 cursor-pointer hover:underline" href={`${head.href}#${href}`}>{text}</p>
 		</Link>
 	))
--- components/sitemap.jsx
+++ components/sitemap.jsx
@@ -51,7 +51,7 @@ export default function Sitemap() {
 			]
 		},
 	]
-	const sitemapJSX = sections.map(({head, links}) => <LinkGroup head={head} links={links} />)
+	const sitemapJSX = sections.map(({head, links}, index) => <LinkGroup key={index} head={head} links={links} />)
 	return (
 		<section className="flex justify-center gap-x-16 mt-7 mb-11 text-base">
 			{sitemapJSX}
