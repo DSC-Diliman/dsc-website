5eb2127395300d4067117468882c66611525428e
Daryll Ko <daryll.ko@gmail.com>
2023-01-28 15:47:54 +0800

    fix: use `next/script` rather than script tag in `next/head`

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,6 @@
 import Head from "next/head"
+import Script from "next/script"
+
 import "../styles/globals.scss"
 import "../styles/calendar.scss"
 
@@ -9,9 +11,9 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="shortcut icon" href="/favicon.png" />
-				<script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
       </Head>
-      {getLayout(<Component {...pageProps}></Component>)}
+			<Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+      { getLayout(<Component {...pageProps}></Component>) }
     </>
   )
 }
