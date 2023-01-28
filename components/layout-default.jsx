1c536a20a3652bb169530616b09e5e61b68ed781
Daryll Ko <daryll.ko@gmail.com>
2023-01-28 17:34:01 +0800

    feat: add `meta` tags to `_app`

--- components/layout-default.jsx
+++ components/layout-default.jsx
@@ -7,6 +7,10 @@ export default function Layout({ children, footerChildren, title }) {
     <>
       <Head>
         <title>{title ? `${title} | GDSC UPD` : "GDSC UP Diliman"}</title>
+				<meta name="description" content="Website for the Google Developer Student Clubs - UP Diliman chapter." />
+				<meta name="keywords" content="Google Developer Student Clubs, GDSC, UP Diliman" />
+				<meta name="author" content="GDSC UP Diliman" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
       <Nav />
