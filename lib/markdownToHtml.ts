abde700c06864da2196a21f3adc923672158b6f1
Daryll Ko <daryll.ko@gmail.com>
2023-01-29 23:30:05 +0800

    fix: add `await`

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -4,7 +4,7 @@ import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = unified()
+  const result = await unified()
     .use(remarkParse)
     .use(remarkRehype)
     .use(rehypeStringify)
