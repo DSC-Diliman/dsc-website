13869ca397e967ad1157335ed8108b76488b0c7f
Daryll Ko <daryll.ko@gmail.com>
2023-01-29 23:29:26 +0800

    refactor: use more stable markdown to HTML pipeline

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,13 @@
 import { unified } from "unified";
-import html from "remark-html";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await unified().use(html).process(markdown);
+  const result = unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeStringify)
+    .process(markdown);
   return result.toString();
 }
