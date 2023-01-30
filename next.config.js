2719d5fa29a9935dd5ba9d998abf4eb9c16646d9
Daryll Ko <daryll.ko@gmail.com>
2023-01-30 12:15:55 +0800

    fix: use `next build` instead of `next export`

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   swcMinify: true,
   compress: true,
-  // The `i18n` option below apparently isn't compatible with `next export`, the command we use
-  // to generate the site. From what I understand, this is a compromise between performance and SEO;
-  // I'll go with the former for now.
-  //
-  // i18n: {
-  //   locales: ["en"],
-  //   defaultLocale: "en",
-  // },
+  // The `i18n` option below and image optimization apparently aren't compatible with `next export`,
+  // the command we currently use to generate the site. I'll use `next start` as our build option for now,
+  // since I believe image optimization is one of the main things that makes our site run smoother.
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 };
