eaaa8745660c50de54df22f100dcd60350cc7ee3
Daryll Ko <daryll.ko@gmail.com>
2023-01-30 12:09:30 +0800

    fix: remove `i18n` option in `next.config.js`

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   swcMinify: true,
   compress: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  // The `i18n` option below apparently isn't compatible with `next export`, the command we use
+  // to generate the site. From what I understand, this is a compromise between performance and SEO;
+  // I'll go with the former for now.
+  //
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
 };
