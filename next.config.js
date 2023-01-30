55ab6d77749c44f355d098246cb9580063d8df7c
Daryll Ko <daryll.ko@gmail.com>
2023-01-30 14:22:43 +0800

    fix: `start` —> `build`

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   swcMinify: true,
   compress: true,
   // The `i18n` option below and image optimization apparently aren't compatible with `next export`,
-  // the command we currently use to generate the site. I'll use `next start` as our build option for now,
+  // the command we currently use to generate the site. I'll use `next build` as our build option for now,
   // since I believe image optimization is one of the main things that makes our site run smoother.
   i18n: {
     locales: ["en"],
