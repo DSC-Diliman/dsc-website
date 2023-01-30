d4cf3a7ba0fd054f33dc42cbd2d7a7ddbc367c87
Daryll Ko <daryll.ko@gmail.com>
2023-01-30 10:46:10 +0800

    fix: add `height` and `width` props to `Portrait`s

--- components/portrait.tsx
+++ components/portrait.tsx
@@ -34,7 +34,7 @@ export default function Portrait({
     <div
       className={`group relative overflow-hidden rounded-xl md:rounded-3xl w-36 h-44 md:w-48 md:h-60 shadow-2xl text-sm img-frame ${className}`}
     >
-      <Image src={src} alt="Member image" className="absolute" fill />
+      <Image src={src} alt="Member image" className="absolute" height={176} width={144} />
       <div className="absolute bottom-0 w-full h-24 bg-gradient-to-t from-black" />
       <div className="relative transition-all mt-28 md:mt-40 group-hover:mt-0 h-full p-2 md:p-4 flex flex-col group-hover:bg-black group-hover:bg-opacity-70 text-white">
         <p className="md:text-base font-medium">{name}</p>
