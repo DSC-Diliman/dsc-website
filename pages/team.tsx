0f654df5ec186bc8386140f65fa246004ece2d25
Daryll Ko <daryll.ko@gmail.com>
2023-02-23 10:14:35 +0800

    fix: make "Meet our Team" font size dynamic

--- pages/team.tsx
+++ pages/team.tsx
@@ -74,7 +74,7 @@ export default function Team({ allMemberData }: Props) {
         <title>Team | GDSC UPD</title>
       </Head>
       <div className="w-full px-10 pt-32 pb-8 text-center">
-        <h1>Meet our Team</h1>
+        <h1 className="text-2xl sm:text-3xl md:text-4xl">Meet our Team</h1>
         <p className="mt-4 text-base">
           The lineup that empowers, enlightens, and nurtures student developers
           in UP Diliman!
