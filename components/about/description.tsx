9a555c245e40128a4a7961380dae156eec599d05
Daryll Ko <daryll.ko@gmail.com>
2023-03-14 12:03:34 +0800

    refactor: replace `ParallaxImage` using framer

--- components/about/description.tsx
+++ components/about/description.tsx
@@ -1,15 +1,26 @@
-import ParallaxImage from "../animations/parallax-image";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+import Image from "next/image";
 
 export default function Description() {
+  const { scrollY } = useScroll();
+  const y = useTransform(scrollY, [0, 300], [0, 75]);
+
   return (
     <section className="flex min-h-screen flex-col items-center justify-center px-10 py-32 text-center">
       <h1 className="text-2xl sm:text-3xl md:mt-0 md:text-4xl">
         What is GDSC-UPD?
       </h1>
-      <ParallaxImage
-        src="images/about-hero.png"
-        className="mx-auto mt-4 mb-12 h-52 w-full max-w-5xl bg-cover sm:mt-4 sm:mb-12 sm:h-64 md:my-6 md:h-80 lg:h-96"
-      />
+      <div className="mb-10 overflow-hidden border-b-2 border-solid border-black">
+        <motion.div className="mx-auto w-full" style={{ y }}>
+          <Image
+            src="/images/about-hero.png"
+            alt="Dot in About page"
+            width={800}
+            height={800}
+          />
+        </motion.div>
+      </div>
       <p className="mb-8 max-w-2xl text-lg md:mb-4 md:max-w-4xl lg:max-w-4xl">
         Ever since its fruition in India, the Developer Student Clubs has grown
         globally and currently has over 1500 chapters spread across over 150
