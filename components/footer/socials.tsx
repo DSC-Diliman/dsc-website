a1b4f93fe8557fb0d27c14d985603b48349b24bf
Daryll Ko <daryll.ko@gmail.com>
2023-03-14 16:54:53 +0800

    fix: use scale animation for social circles

--- components/footer/socials.tsx
+++ components/footer/socials.tsx
@@ -1,8 +1,11 @@
+import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-import ParallaxObject from "../animations/parallax-object";
 
 export default function Socials() {
+  const { scrollY } = useScroll();
+  const y = useTransform(scrollY, [0, 300], [0, 75]);
+
   const socials = [
     {
       name: "Facebook",
@@ -20,46 +23,51 @@ export default function Socials() {
       link: "mailto:dscdiliman@gmail.com",
     },
   ];
+
+  const socialsJSX = socials.map((e, index) => (
+    <Link
+      className="relative"
+      key={index}
+      href={e.link}
+      target="_blank"
+      rel="noreferrer"
+    >
+      <button className="div-style1 md:pb-4.5 w-full rounded-3xl px-6 pb-4 pt-5 text-center shadow-none drop-shadow-lg transition-all hover:scale-110 md:px-9 md:pt-6">
+        <Image
+          src={e.icon}
+          alt="Event icon"
+          height={60}
+          width={60}
+          className="mx-auto mb-4 h-6 md:h-8"
+        />
+        <p className="text-md">{e.name}</p>
+      </button>
+    </Link>
+  ));
+
   return (
-    <section className="relative">
-      <div className="mx-auto grid max-w-max grid-cols-2 gap-y-3 gap-x-6 lg:grid-cols-3 lg:gap-8">
-        <div className="relative flex flex-col justify-center lg:col-span-2">
-          <ParallaxObject
-            className="absolute -left-6 -z-10 h-20 w-20 rounded-full bg-amber-300/50"
-            y={[0, 200]}
-          />
-          <ParallaxObject
-            className="absolute left-12 -z-10 h-10 w-10 rounded-full bg-blue-300/50"
-            y={[200, 400]}
-          />
-          <ParallaxObject
-            className="absolute left-36 -z-10 h-10 w-10 rounded-full bg-red-300/50"
-            y={[0, 100]}
-          />
-          <h1 className="relative text-3xl">Reach us</h1>
-          <p className="relative text-base">in our socials!</p>
+    <section className="relative mx-auto max-w-max">
+      <motion.div
+        className="absolute -left-10 top-0 -z-10 h-20 w-20 rounded-full bg-amber-300/50"
+        animate={{ scale: [1, 1.1, 1] }}
+        transition={{ repeat: Infinity, duration: 3 }}
+      />
+      <motion.div
+        className="absolute left-14 top-72 -z-10 h-12 w-12 rounded-full bg-blue-300/50"
+        animate={{ scale: [1, 1.1, 1] }}
+        transition={{ repeat: Infinity, duration: 3 }}
+      />
+      <motion.div
+        className="absolute left-36 top-20 -z-10 h-14 w-14 rounded-full bg-red-300/50"
+        animate={{ scale: [1, 1.1, 1] }}
+        transition={{ repeat: Infinity, duration: 3 }}
+      />
+      <div className="grid grid-cols-2 gap-y-3 gap-x-6 lg:gap-8">
+        <div className="flex flex-col justify-center">
+          <h1 className="text-3xl">Reach us</h1>
+          <p className="text-base">in our socials!</p>
         </div>
-        <div className="hidden lg:block" />
-        {socials.map((e, index) => (
-          <Link
-            className="relative"
-            key={index}
-            href={e.link}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <button className="div-style1 md:pb-4.5 w-full rounded-3xl px-6 pb-4 pt-5 text-center shadow-none drop-shadow-lg transition-all hover:scale-110 md:px-9 md:pt-6">
-              <Image
-                src={e.icon}
-                alt="Event icon"
-                height={60}
-                width={60}
-                className="mx-auto mb-4 h-6 md:h-8"
-              />
-              <p className="text-md">{e.name}</p>
-            </button>
-          </Link>
-        ))}
+        {socialsJSX}
       </div>
     </section>
   );
